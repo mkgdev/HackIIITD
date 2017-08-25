d72acbc1583094e828f16fe6378d005134d76346
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2017-08-25 23:31:21 +0530

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var controller = require('./routes/controller.js');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use('view engine','ejs');
+app.use(express.static(__dirname + '/public');
+app.use(methodOverride("_method"));
+app.use(controller);
+
+//listening routes
+
+app.listen(2000,()=>{
+  console.log('Magic happens on port 3000');
+})
